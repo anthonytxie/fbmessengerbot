79ded89eb49a5820de10872a289acb2a869ce7ca
anthonytxie <anthony.xie@vidyard.com>
2017-07-10 19:51:38 -0400

    added verify token

--- server/app.js
+++ server/app.js
@@ -11,4 +11,16 @@ const app = express();
 app.use(bodyParser.json())
 app.use(routes)
 
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'catfish') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 module.exports = app
