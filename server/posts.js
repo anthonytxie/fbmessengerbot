8f7a5c92077cd7ee004f1d579aacc857a8057f75
anthonytxie <anthony.xie@vidyard.com>
2017-07-11 19:26:39 -0400

    added stuff

--- server/posts.js
+++ server/posts.js
@@ -1,73 +1,51 @@
+const request = require('request');
 
 
+const messengerPosts = {};
 
-// set greetings
-curl -X POST -H "Content-Type: application/json" -d '{
-  "setting_type":"greeting",
-  "greeting":{
-    "text":"Hi {{user_first_name}}, welcome to this bot."
-  }
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAACH5pouINQBANbCRlN4XCgcSa1wEEcdvGcIgZCrFjbBdVq9gGpZAHkg8FPDjZBuyVMxxLycu2ZChXVLJClAi2e86kGfSbpmcVEO3uVp2VUTaZBZB8frZC1lAooy4ds43xCQyivyZAoy9tvu7WDBc9ksTOXvATTDeBZCexwv4RO3UbAZDZD"    
-
-
-
-// get started button
-
-
-curl -X POST -H "Content-Type: application/json" -d '{ 
-  "get_started":{
-    "payload":"GET_STARTED_PAYLOAD"
-  }
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAACH5pouINQBANbCRlN4XCgcSa1wEEcdvGcIgZCrFjbBdVq9gGpZAHkg8FPDjZBuyVMxxLycu2ZChXVLJClAi2e86kGfSbpmcVEO3uVp2VUTaZBZB8frZC1lAooy4ds43xCQyivyZAoy9tvu7WDBc9ksTOXvATTDeBZCexwv4RO3UbAZDZD"  
-
-// get payload on get started button
-
-curl -X GET "https://graph.facebook.com/v2.6/me/messenger_profile?fields=get_started&access_token=PAGE_ACCESS_TOKEN=EAACH5pouINQBANbCRlN4XCgcSa1wEEcdvGcIgZCrFjbBdVq9gGpZAHkg8FPDjZBuyVMxxLycu2ZChXVLJClAi2e86kGfSbpmcVEO3uVp2VUTaZBZB8frZC1lAooy4ds43xCQyivyZAoy9tvu7WDBc9ksTOXvATTDeBZCexwv4RO3UbAZDZD"    
-
-// post persistent menu
-
-curl -X POST -H "Content-Type: application/json" -d '{
-  "persistent_menu":[
-    {
-      "locale":"default",
-      "composer_input_disabled":true,
-      "call_to_actions":[
-        {
-          "title":"Menu",
-          "type":"nested",
-          "call_to_actions":[
-            {
-              "title":"Burger",
-              "type":"postback",
-              "payload":"menu-burger"
-            },
-            {
-              "title":"Drinks",
-              "type":"postback",
-              "payload":"menu-drinks"
-            },
-            {
-              "title":"Sides",
-              "type":"postback",
-              "payload":"menu-sides"
-            }
-          ]
-        },
-        {
-          "type":"postback",
-          "title":"Order",
-          "payload":"order",
-        },
-        {
-          "type":"postback",
-          "title":"Help",
-          "payload":"help",
-        }
-      ]
-    },
-    {
-      "locale":"zh_CN",
-      "composer_input_disabled":false
+messengerPosts.setGreetings = (text) => {
+  request({
+    uri: "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAACH5pouINQBANbCRlN4XCgcSa1wEEcdvGcIgZCrFjbBdVq9gGpZAHkg8FPDjZBuyVMxxLycu2ZChXVLJClAi2e86kGfSbpmcVEO3uVp2VUTaZBZB8frZC1lAooy4ds43xCQyivyZAoy9tvu7WDBc9ksTOXvATTDeBZCexwv4RO3UbAZDZD",
+    method: 'POST',
+    json: {  
+      "setting_type":"greeting",
+      "greeting":{"text":text}
     }
-  ]
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAACH5pouINQBANbCRlN4XCgcSa1wEEcdvGcIgZCrFjbBdVq9gGpZAHkg8FPDjZBuyVMxxLycu2ZChXVLJClAi2e86kGfSbpmcVEO3uVp2VUTaZBZB8frZC1lAooy4ds43xCQyivyZAoy9tvu7WDBc9ksTOXvATTDeBZCexwv4RO3UbAZDZD"
+  },(error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    } 
+    else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+};
+
+// setGreetings("Hi {{user_first_name}}, welcome to this bot.");
+
+
+messengerPosts.getStarted = (payload) => {
+  request({
+    uri: "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAACH5pouINQBANbCRlN4XCgcSa1wEEcdvGcIgZCrFjbBdVq9gGpZAHkg8FPDjZBuyVMxxLycu2ZChXVLJClAi2e86kGfSbpmcVEO3uVp2VUTaZBZB8frZC1lAooy4ds43xCQyivyZAoy9tvu7WDBc9ksTOXvATTDeBZCexwv4RO3UbAZDZD",
+    method: 'POST',
+    json: 
+      { 
+        "get_started":{
+          "payload": payload
+        }
+      }}
+    ,(error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      } 
+      else {
+        console.error("Unable to send message.");
+        console.error(response);
+        console.error(error);
+    }
+  });  
+};
+
+messengerPosts.getStarted('start')
