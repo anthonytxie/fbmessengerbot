e2ecf12fc858ffbd568731379081b5a34fc33b99
anthonytxie <anthony.xie@vidyard.com>
2017-07-11 00:26:57 -0400

    added request

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const request = require('request');
 
 const app = express();
 
