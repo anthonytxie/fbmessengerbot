d8d413642ce962a1175d8c05d06aa74dac939d04
anthonytxie <anthony.xie@vidyard.com>
2017-07-11 21:14:37 -0400

    asdf

--- server/send-functions.js
+++ server/send-functions.js
@@ -67,7 +67,7 @@ function sendTextMessage(recipientId, messageText) {
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: 'EAACH5pouINQBAJPuNzZCbJsOg18p2jUZCedPv8iwbcgfbV3wpStt0ObdmRJQzd3QbZBCVwztvbOHCTDmQ9AhjEHGKJz89uNkH1oSnWYh93l3DCGeFpjvsaMk0BeZA172eSaZAGA6YvAEBb7JVtZBBbJtlMNjgnZAUdOFjFrtzdLXAZDZD' },
+    qs: { access_token: 'EAACH5pouINQBAJPuNzZCbJsOg18p2jUZCedPv8iwbcgfbV3wpStt0ObdmRJQzd3QbZBCVwztvbOHCTDmQ9AhjEHGKJz89uNkH1oSnWYh93l3DCGeFpjvsaMk0BeZA172eSaZAGA6YvAEBb7JVtZBBbJtlMNjgnZAUdOFjFrtzdLXAZDZD'},
     method: 'POST',
     json: messageData
 
